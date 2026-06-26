9b94e8bdf7d355736ac18d7c65405d4b94168eb2
Jack <jack11computer@gmail.com>
2026-06-26 17:45:00 +0700

    Implement AEP registry storage

--- applications/external/astraeon/sdk/src/astra_registry.c
+++ applications/external/astraeon/sdk/src/astra_registry.c
@@ -1,16 +1,52 @@
 #include "astra_registry.h"
+#include <stdbool.h>
+#include <string.h>
+
+#define ASTRA_REGISTRY_MAX_ENTRIES 16
+
+typedef struct {
+    AstraEventType type;
+    AstraEventHandler handler;
+    bool used;
+} AstraRegistryEntry;
+
+static AstraRegistryEntry registry[ASTRA_REGISTRY_MAX_ENTRIES];
 
 AstraResult astra_registry_init(void) {
+    memset(registry, 0, sizeof(registry));
     return astra_result_ok();
 }
 
 AstraResult astra_registry_register(AstraEventType type, AstraEventHandler handler) {
-    (void)type;
-    (void)handler;
-    return astra_result_ok();
+    if(!handler) {
+        return astra_result_error(AstraStatusInvalidArgument, "handler is null");
+    }
+
+    for(size_t index = 0; index < ASTRA_REGISTRY_MAX_ENTRIES; index++) {
+        if(registry[index].used && registry[index].type == type) {
+            registry[index].handler = handler;
+            return astra_result_ok();
+        }
+    }
+
+    for(size_t index = 0; index < ASTRA_REGISTRY_MAX_ENTRIES; index++) {
+        if(!registry[index].used) {
+            registry[index].used = true;
+            registry[index].type = type;
+            registry[index].handler = handler;
+            return astra_result_ok();
+        }
+    }
+
+    return astra_result_error(AstraStatusBusy, "registry is full");
 }
 
 AstraEventHandler astra_registry_find(AstraEventType type) {
-    (void)type;
+    for(size_t index = 0; index < ASTRA_REGISTRY_MAX_ENTRIES; index++) {
+        if(registry[index].used && registry[index].type == type) {
+            return registry[index].handler;
+        }
+    }
+
     return 0;
 }
--- applications/external/astraeon/tests/runtime/test_registry.c
+++ applications/external/astraeon/tests/runtime/test_registry.c
@@ -6,8 +6,17 @@ static AstraResult astra_test_dummy_handler(AstraEvent* event) {
     return astra_result_ok();
 }
 
+static AstraResult astra_test_replacement_handler(AstraEvent* event) {
+    (void)event;
+    return astra_result_ok();
+}
+
 bool astra_test_registry(void) {
     if(astra_registry_init().status != AstraStatusOk) {
+        return false;
+    }
+
+    if(astra_registry_find(AstraEventTypeStatus) != 0) {
         return false;
     }
 
@@ -15,5 +24,21 @@ bool astra_test_registry(void) {
         return false;
     }
 
-    return astra_registry_find(AstraEventTypeStatus) == 0;
+    if(astra_registry_find(AstraEventTypeStatus) != astra_test_dummy_handler) {
+        return false;
+    }
+
+    if(astra_registry_register(AstraEventTypeStatus, astra_test_replacement_handler).status != AstraStatusOk) {
+        return false;
+    }
+
+    if(astra_registry_find(AstraEventTypeStatus) != astra_test_replacement_handler) {
+        return false;
+    }
+
+    if(astra_registry_register(AstraEventTypeStatus, 0).status != AstraStatusInvalidArgument) {
+        return false;
+    }
+
+    return true;
 }
