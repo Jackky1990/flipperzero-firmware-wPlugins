658a55399a275a178f8e4007193360ba525cdd79
Jack <jack11computer@gmail.com>
2026-06-27 19:20:21 +0700

    Add runtime loop context API

--- applications/external/astraeon/sdk/include/astra_runtime_loop.h
+++ applications/external/astraeon/sdk/include/astra_runtime_loop.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "astra_result.h"
+#include "astra_runtime_context.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,6 +10,15 @@ extern "C" {
 AstraResult astra_runtime_loop_init(void);
 AstraResult astra_runtime_loop_step(void);
 AstraResult astra_runtime_loop_shutdown(void);
+
+AstraResult astra_runtime_loop_init_context(
+    AstraRuntimeContext* context);
+
+AstraResult astra_runtime_loop_step_context(
+    AstraRuntimeContext* context);
+
+AstraResult astra_runtime_loop_shutdown_context(
+    AstraRuntimeContext* context);
 
 #ifdef __cplusplus
 }
