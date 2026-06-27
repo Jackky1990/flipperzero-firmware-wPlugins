0e6400722c879c7ace483c6800f90c3538691570
Jack <jack11computer@gmail.com>
2026-06-27 22:05:33 +0700

    Use default runtime context in event bus wrappers

--- applications/external/astraeon/sdk/src/astra_event_bus.c
+++ applications/external/astraeon/sdk/src/astra_event_bus.c
@@ -1,16 +1,13 @@
 #include "astra_event_bus.h"
 #include "astra_dispatcher.h"
+#include "astra_runtime_default.h"
 
 AstraResult astra_event_bus_init(void) {
-    return astra_dispatcher_init();
+    return astra_event_bus_init_context(astra_runtime_default_context());
 }
 
 AstraResult astra_event_bus_publish(AstraEvent* event) {
-    if(!event) {
-        return astra_result_error(AstraStatusInvalidArgument, "event is null");
-    }
-
-    return astra_dispatcher_dispatch(event);
+    return astra_event_bus_publish_context(astra_runtime_default_context(), event);
 }
 
 AstraResult astra_event_bus_init_context(AstraRuntimeContext* context) {
