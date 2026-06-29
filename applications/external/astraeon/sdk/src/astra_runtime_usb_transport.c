c3032a476b3b54bc3b7e3874e6fd4780151c3bc1
Jack <jack11computer@gmail.com>
2026-06-29 08:30:25 +0700

    Add runtime USB CDC config flag

--- applications/external/astraeon/sdk/src/astra_runtime_usb_transport.c
+++ applications/external/astraeon/sdk/src/astra_runtime_usb_transport.c
@@ -1,6 +1,7 @@
 #include "astra_runtime_usb_transport.h"
+#include "astra_runtime_config.h"
 
-#if defined(ASTRA_RUNTIME_ENABLE_USB_CDC)
+#if ASTRA_RUNTIME_ENABLE_USB_CDC
 #include <furi_hal_usb_cdc.h>
 #define ASTRA_USB_CDC_CHANNEL 0u
 #endif
@@ -10,7 +11,7 @@ static AstraResult astra_usb_transport_send(const uint8_t* data, size_t size) {
         return astra_result_error(AstraStatusInvalidArgument, "usb send invalid");
     }
 
-#if defined(ASTRA_RUNTIME_ENABLE_USB_CDC)
+#if ASTRA_RUNTIME_ENABLE_USB_CDC
     if(size > 64) {
         return astra_result_error(AstraStatusBusy, "usb payload too large");
     }
@@ -29,7 +30,7 @@ static AstraResult astra_usb_transport_receive(uint8_t* buffer, size_t buffer_si
 
     *received_size = 0;
 
-#if defined(ASTRA_RUNTIME_ENABLE_USB_CDC)
+#if ASTRA_RUNTIME_ENABLE_USB_CDC
     uint16_t max_len = buffer_size > 64 ? 64 : (uint16_t)buffer_size;
     int32_t received = furi_hal_cdc_receive(ASTRA_USB_CDC_CHANNEL, buffer, max_len);
 
