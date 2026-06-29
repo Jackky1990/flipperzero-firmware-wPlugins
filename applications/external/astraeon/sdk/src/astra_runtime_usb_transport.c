62b3a0e47962591c390b4f48e583813b0e7a96c1
Jack <jack11computer@gmail.com>
2026-06-29 08:09:59 +0700

    Prepare USB CDC runtime transport adapter

--- applications/external/astraeon/sdk/src/astra_runtime_usb_transport.c
+++ applications/external/astraeon/sdk/src/astra_runtime_usb_transport.c
@@ -1,11 +1,25 @@
 #include "astra_runtime_usb_transport.h"
 
+#if defined(ASTRA_RUNTIME_ENABLE_USB_CDC)
+#include <furi_hal_usb_cdc.h>
+#define ASTRA_USB_CDC_CHANNEL 0u
+#endif
+
 static AstraResult astra_usb_transport_send(const uint8_t* data, size_t size) {
     if(!data || size == 0) {
         return astra_result_error(AstraStatusInvalidArgument, "usb send invalid");
     }
 
+#if defined(ASTRA_RUNTIME_ENABLE_USB_CDC)
+    if(size > 64) {
+        return astra_result_error(AstraStatusBusy, "usb payload too large");
+    }
+
+    furi_hal_cdc_send(ASTRA_USB_CDC_CHANNEL, (uint8_t*)data, (uint16_t)size);
+    return astra_result_ok();
+#else
     return astra_result_error(AstraStatusNotFound, "usb transport not attached");
+#endif
 }
 
 static AstraResult astra_usb_transport_receive(uint8_t* buffer, size_t buffer_size, size_t* received_size) {
@@ -14,7 +28,20 @@ static AstraResult astra_usb_transport_receive(uint8_t* buffer, size_t buffer_si
     }
 
     *received_size = 0;
+
+#if defined(ASTRA_RUNTIME_ENABLE_USB_CDC)
+    uint16_t max_len = buffer_size > 64 ? 64 : (uint16_t)buffer_size;
+    int32_t received = furi_hal_cdc_receive(ASTRA_USB_CDC_CHANNEL, buffer, max_len);
+
+    if(received <= 0) {
+        return astra_result_error(AstraStatusNotFound, "usb no data");
+    }
+
+    *received_size = (size_t)received;
+    return astra_result_ok();
+#else
     return astra_result_error(AstraStatusNotFound, "usb transport not attached");
+#endif
 }
 
 AstraResult astra_runtime_usb_transport_init(AstraRuntimeTransport* transport) {
