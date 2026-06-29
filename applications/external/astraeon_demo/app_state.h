b56919f66b8cdc329d537414676fcb3a9680eb83
Jack <jack11computer@gmail.com>
2026-06-29 21:39:24 +0700

    Embed ASTRAEON application context

--- applications/external/astraeon_demo/app_state.h
+++ applications/external/astraeon_demo/app_state.h
@@ -3,8 +3,10 @@
 #include <furi.h>
 #include <gui/gui.h>
 #include <input/input.h>
+#include "context/app_context.h"
 
 typedef struct {
+    AstraeonApplication app;
     FuriMessageQueue* queue;
     ViewPort* view_port;
     Gui* gui;
