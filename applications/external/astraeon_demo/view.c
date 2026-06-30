2644cad9c4715fcac1f9ffd3b45db0cdfe094415
Jack <jack11computer@gmail.com>
2026-06-30 09:12:29 +0700

    Render ASTRAEON menu from screen manager

--- applications/external/astraeon_demo/view.c
+++ applications/external/astraeon_demo/view.c
@@ -1,5 +1,6 @@
 #include "view.h"
 #include "app_state.h"
+#include "screen/screen_manager.h"
 
 #include <stdio.h>
 
@@ -10,20 +11,18 @@ void astraeon_demo_draw(Canvas* canvas, void* context) {
     canvas_set_font(canvas, FontPrimary);
     canvas_draw_str_aligned(canvas, 64, 10, AlignCenter, AlignCenter, "ASTRAEON-X");
 
-    const char* items[] = {
-        "Status",
-        "GPIO",
-        "NFC",
-        "RFID",
-        "SubGHz",
-    };
-
     canvas_set_font(canvas, FontSecondary);
     canvas_draw_str_aligned(canvas, 64, 23, AlignCenter, AlignCenter, app->ping_pass ? "Runtime: READY" : "Runtime: FAIL");
 
-    for(uint8_t i = 0; i < 5; i++) {
+    uint8_t count = astraeon_screen_count();
+    for(uint8_t i = 0; i < count; i++) {
         char line[32];
-        snprintf(line, sizeof(line), "%c %s", app->app.ui.menu_index == i ? '>' : ' ', items[i]);
+        snprintf(
+            line,
+            sizeof(line),
+            "%c %s",
+            app->app.ui.current_screen == i ? '>' : ' ',
+            astraeon_screen_title((AstraeonScreen)i));
         canvas_draw_str(canvas, 22, 36 + (i * 9), line);
     }
 }
